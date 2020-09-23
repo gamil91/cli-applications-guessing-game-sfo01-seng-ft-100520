50ca232152f744bc3bdf288d412912a5d0484432
Alexa Gamil <alexis_gamil99@yahoo.com>
2020-09-23 20:15:56 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -3,7 +3,7 @@ require 'pry'
 def run_guessing_game
   number = rand(6) + 1 
   puts "Guess a number between 1 and 6!"
-  input = gets.chomp  
+  input = gets.chomp.  
 binding.pry
     if input == number
       p "You guessed the correct number!"
